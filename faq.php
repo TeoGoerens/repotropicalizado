732b0daa69533f81bfbfe5f03fdceb2149fa6483
Franvillada <franciscovilladaa@gmail.com>
2018-08-31 18:11:32 -0300

    Ahi subo el wireframe del Home, fijate que te parece

---
+++ faq.php
@@ -0,0 +1,32 @@
+<div class="row">
+
+    <div class="col-lg-12">
+        <ol>
+            <li>
+
+            </li>
+            <li>
+                
+            </li>
+            <li>
+                
+            </li>
+            <li>
+                
+            </li>
+            <li>
+                
+            </li>
+            <li>
+                
+            </li>
+            <li>
+                
+            </li>
+            <li>
+                
+            </li>
+        </ol>    
+    </div>
+
+</div>
