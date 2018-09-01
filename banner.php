3ceab3d3ab85d86bcbaf4195ffd721cf931e3610
TeoGoerens <goerens_teo@hotmail.com>
2018-09-01 17:45:24 -0300

    Completo el banner

--- banner.php
+++ banner.php
@@ -1,8 +1,15 @@
 <div class="row banner-container">
-    <div class="col-lg-4 offset-2 info-general">
-        
+    <div class="col-lg-5 offset-1 info-general">
+        <h2>Bienvenido a <strong>SoundClub</strong></h2>
+        <article>
+            <p>Animate a formar parte de este club exclusivo y obtené toda la información del mundo de la música.</p>
+            <p>Compartí tu talento, creá tu red de contactos, informate sobre los eventos más importantes y viví nuevas experiencias.</p>
+        </article>
+        <button class="buttonbanner"><a href="#FAQ">Conoce más</a></button>
+         
+    
     </div>
-    <div class="col-lg-4 registro">
+    <div class="col-lg-4 offset-1 registro">
         
         <?php
             include("formulario-registro.php")
--- faq.php
+++ faq.php
@@ -1,7 +1,7 @@
 <div class="row faq-container">
 
     <div class="col-lg-12">
-        <h2 class="titulo-faq">Preguntas Frecuentes</h2>
+        <h2 class="titulo-faq" id="FAQ">Preguntas Frecuentes</h2>
         <ul class="lista-faq">
             <li>
                 <a href="#" class="preguntas"><p>¿Cómo creo una cuenta de SoundClub?</p></a>
